f8bdb9ed7d4b55c1cd35cf1e7367af8e0a5ab429
Jie Zhang <zhangjiew@gmail.com>
2022-01-15 13:47:44 -0700

    amdmin can add, delete, update categories

--- back-end/controller/categoryControllers.js
+++ back-end/controller/categoryControllers.js
@@ -12,7 +12,33 @@ const categoryCtrl = {
 
   createCategories: async (req, res) => {
     try {
-      res.json("check admin success");
+      // only admin can create, delete and update category
+      const { name } = req.body;
+      const category = await Category.findOne({ name });
+      if (category)
+        return res.status(400).json({ msg: "This category already exists" });
+      const newCategory = new Category({ name });
+      await newCategory.save();
+
+      res.json({ msg: "Category created" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+
+  deleteCategories: async (req, res) => {
+    try {
+      await Category.findByIdAndDelete(req.params.id);
+      res.json({ msg: "Category deleted" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  updateCategories: async (req, res) => {
+    try {
+      const { name } = req.body;
+      await Category.findByIdAndUpdate({ _id: req.params.id }, { name });
+      res.json({ msg: "Category Updated" });
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
--- back-end/routes/categoryRoutes.js
+++ back-end/routes/categoryRoutes.js
@@ -6,4 +6,7 @@ const authAdmin = require("../middleware/authAdmin");
 router.get("/", categoryCtrl.getCategories);
 router.post("/", auth, authAdmin, categoryCtrl.createCategories);
 
+router.delete("/:id", auth, authAdmin, categoryCtrl.deleteCategories);
+router.put("/:id", auth, authAdmin, categoryCtrl.updateCategories);
+
 module.exports = router;
