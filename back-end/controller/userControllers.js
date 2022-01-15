b0903aba62cde9350aca26eee61b6235b8b64489
Jie Zhang <zhangjiew@gmail.com>
2022-01-14 19:32:15 -0700

    added accesstoken, refreshtoken, cookies with a user logged in or registered

--- back-end/controller/userControllers.js
+++ back-end/controller/userControllers.js
@@ -30,17 +30,33 @@ const userCtrl = {
 
       res.cookie("refreshtoken", refreshtoken, {
         httpOnly: true,
+        secure: false,
         path: "/user/refresh_token",
       });
       res.json({ asscesstoken });
-      //   res.json({ msg: "Register Success" });
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
   },
   refreshToken: (req, res) => {
-    const rf_token = req.cookies.refreshtoken;
-    res.json({ rf_token });
+    try {
+      const rf_token = req.cookies.refreshtoken;
+
+      if (!rf_token)
+        return res.status(400).json({ msg: "Please Login or Register" });
+      jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err)
+          return res.status(400).json({ msg: "Please Login or Register" });
+
+        const accesstoken = createAccessToken({ id: user.id });
+
+        res.json({ accesstoken });
+      });
+
+      res.json({ rf_token });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
   },
 };
 
--- back-end/server.js
+++ back-end/server.js
@@ -5,15 +5,15 @@ const PORT = process.env.PORT || 5000;
 const connectDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
-// const cors = require("cors");
+const cors = require("cors");
 const fileUpload = require("express-fileupload");
-// const cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 
 connectDB();
 const app = express();
 app.use(express.json());
-// app.use(cookieParser);
-// app.use(cors);
+app.use(cookieParser());
+app.use(cors());
 app.use(
   fileUpload({
     useTempFiles: true,
