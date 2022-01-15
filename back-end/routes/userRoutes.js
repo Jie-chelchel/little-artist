ac6f837b0a273dbeb314064a0cdc4dc4f07b8e5e
Jie Zhang <zhangjiew@gmail.com>
2022-01-14 22:51:46 -0700

    added auth middleware

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controller/userControllers");
-
+const auth = require("../middleware/auth");
 router.post("/register", userCtrl.register);
 router.post("/login", userCtrl.login);
 router.get("/logout", userCtrl.logout);
 router.get("/refresh_token", userCtrl.refreshToken);
+router.get("/infor", auth, userCtrl.getUser);
 module.exports = router;
