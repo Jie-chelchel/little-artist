1341b42dba962a05f3e92aaf21008c8178ee7841
Jie Zhang <zhangjiew@gmail.com>
2022-01-16 14:39:35 -0700

    use useContext to control state

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,44 +1,16 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import ProductPage from "./pages/ProductPage";
-import Navbar from "./components/Navbar";
-import Backdrop from "./components/Backdrop";
-import SideDrawer from "./components/SideDrawer";
-import { useState } from "react";
+import { DataProvider } from "./GlobalState";
+import Headers from "./components/headers/Header";
+import MainPages from "./components/mainpages/pages";
 
 function App() {
-  const [sideToggle, setSideToggle] = useState(false);
   return (
-    <Router>
-      <Navbar
-        click={() => {
-          setSideToggle(true);
-        }}
-      />
-      <Backdrop
-        show={sideToggle}
-        click={() => {
-          setSideToggle(false);
-        }}
-      />
-      <SideDrawer
-        show={sideToggle}
-        click={() => {
-          setSideToggle(false);
-        }}
-      />
-      <main>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/products/:id" element={<ProductPage />} />
-          <Route exact path="/cart" element={<Cart />} />
-          <Route />
-          <Route />
-        </Routes>
-      </main>
-    </Router>
+    <DataProvider>
+      <Router>
+        <Headers />
+        <MainPages />
+      </Router>
+    </DataProvider>
   );
 }
 
---
+++ front-end/src/components/headers/Header.js
@@ -0,0 +1,9 @@
+import React, { useState, useContext } from "react";
+import { GlobalState } from "../../GlobalState";
+
+function Header() {
+  const value = useContext(GlobalState);
+  return <header></header>;
+}
+
+export default Header;
