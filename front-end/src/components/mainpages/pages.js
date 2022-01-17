e8c559d0476dd8448bbc0c8c5e80c0f7218f53f9
Jie Zhang <zhangjiew@gmail.com>
2022-01-17 15:26:22 -0700

    added not found page

--- front-end/src/components/mainpages/pages.js
+++ front-end/src/components/mainpages/pages.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import Cart from "./cart/Cart";
+import Products from "./products/Products";
+import { Routes, Route } from "react-router-dom";
+import NotFound from "./utils/notFound/Notfound";
 
 function MainPages() {
-  return <div>MainPages</div>;
+  return (
+    <Routes>
+      <Route path="/" exact element={<Products />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/register" exact element={<Register />} />
+      <Route path="/cart" exact element={<Cart />} />
+
+      <Route path="*" exact element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default MainPages;
