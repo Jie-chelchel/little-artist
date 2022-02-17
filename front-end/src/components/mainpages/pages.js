cb9b34479c8611e7de62aa09b9dc722e41b4463b
Jie Zhang <zhangjiew@gmail.com>
2022-02-16 23:29:24 -0700

    admin can edit user profile at frontend

--- front-end/src/components/mainpages/pages.js
+++ front-end/src/components/mainpages/pages.js
@@ -9,10 +9,11 @@ import { useSelector } from "react-redux";
 import ForgotPassword from "./auth/ForgotPassword";
 import ResetPassword from "./auth/ResetPassword";
 import Profile from "./profile/Profile";
+import EditUser from "./profile/EditUser";
 
 function MainPages() {
   const auth = useSelector((state) => state.auth);
-  const { isLoggedIn } = auth;
+  const { isLoggedIn, isAdmin } = auth;
   return (
     <Switch>
       <Route path="/login" exact component={isLoggedIn ? NotFound : Login} />
@@ -29,6 +30,11 @@ function MainPages() {
       <Route path="/forgot_password" exact component={ForgotPassword} />
       <Route path="/user/reset/:token" exact component={ResetPassword} />
       <Route path="/profile" exact component={Profile} />
+      <Route
+        path="/edit_user/:id"
+        exact
+        component={isAdmin ? EditUser : NotFound}
+      />
 
       {/* <Route path="/" exact component={Products} /> */}
       {/* <Route path="/productdetail/:id" exact component={ProductDetail} /> */}
